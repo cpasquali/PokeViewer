375393772d0e2bc11fe89807ec443ea594326fe4
Constantino Pasquali <163750560+cpasquali@users.noreply.github.com>
2024-08-04 20:23:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,17 +8,36 @@ import { PokemonDetails } from "./components/Pokemons/PokemonDetails";
 import { PokemonList } from "./components/Pokemons/PokemonList";
 
 function App() {
+  const themeSave = () => {
+    const data = localStorage.getItem("theme");
+    return data ? JSON.parse(data) : "light";
+  };
+
   const [type, setType] = useState(null);
   const [searchPokemon, setSearchPokemon] = useState(null);
-  const [theme, setTheme] = useState("light")
+  const [theme, setTheme] = useState(themeSave);
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
   return (
     <>
-      <Navbar setType={setType} setSearchPokemon={setSearchPokemon} setTheme={setTheme} theme={theme}/>
+      <Navbar
+        setType={setType}
+        setSearchPokemon={setSearchPokemon}
+        setTheme={setTheme}
+        theme={theme}
+      />
       <Switch>
         <Route
           path="/"
           component={() => (
-            <PokemonList type={type} searchPokemon={searchPokemon} theme={theme}/>
+            <PokemonList
+              type={type}
+              searchPokemon={searchPokemon}
+              theme={theme}
+            />
           )}
         />
         <Route path="/pokemon/:id" component={PokemonDetails} />
